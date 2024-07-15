80dd75a9400b46074c57d94f1f7ef121d46dc975
matthewblu421 <44147556+matthewblu421@users.noreply.github.com>
2024-07-15 17:11:31 -0400

    In the last commit I mean

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
+    <div >
      This is the landing page.
     </div>
   )
