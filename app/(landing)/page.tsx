a38bd7490bde4266691728f83cb299ac13d6c212
matthewblu421 <44147556+matthewblu421@users.noreply.github.com>
2024-07-15 17:12:27 -0400

    In the last commit I mean 'route groups'

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <div >
-     This is the landing page.
+     This is the landing page. 
     </div>
   )
 }
