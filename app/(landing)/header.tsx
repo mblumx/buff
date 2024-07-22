adab3296595d895fcae0af1b7f659d52deac667e
matthewblu421 <44147556+matthewblu421@users.noreply.github.com>
2024-07-22 13:06:50 -0400

    Loader added to header. Sign in button added to header

--- app/(landing)/header.tsx
+++ app/(landing)/header.tsx
@@ -1,20 +1,41 @@
+import { Button } from "@/components/ui/button"
+import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
+import { Loader } from "lucide-react"
 import Image from "next/image"
 
 //using named export instead of default export because it is a component, not a page
 export const Header = () => {
     return (
       <header className="h-20 w-full border-b-2 border-slate-200 px-4">
-        <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full bg-red-500">
+        <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
           {/* <div className="flex items-center justify-between pl-2 font-bold text-white uppercase"> */}
           <div className="pt-8 pl-4 pb-7 flex items-center gap-x-2 ">
             <Image
               src={"/popcorn.svg"}
               alt="Popcorn Logo"
-              height={35}
-              width={35}
+              height={42}
+              width={42}
             />
-            <h1 className="text-2xl font-extrabold text-white tracking-wide">Buff</h1>
+            <h1 className="text-2xl font-extrabold text-slate-700 tracking-wide">Buff</h1>
           </div>
+          <ClerkLoading>
+            <Loader
+            className="h-5 w-5 text-muted-foreground animate-spin"/>
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedIn>
+            <UserButton>
+
+            </UserButton>
+            </SignedIn>
+            <SignedOut>
+            <SignInButton mode="modal">
+                <Button size="lg" variant="signIn">
+                    Sign in
+                </Button>
+            </SignInButton>
+            </SignedOut>
+          </ClerkLoaded>
         </div>
       </header>
     )
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,27 @@
-import type { Metadata } from "next";
-import { Inter, Open_Sans } from "next/font/google";
-import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next"
+import {   Inter  } from "next/font/google"
+import "./globals.css"
+import { ClerkProvider } from "@clerk/nextjs"
 
+{
+  /* To use Roboto (font):  
+const font = Roboto({ subsets: ["latin"], weight: ["100",  "300", "400", "500"] });
+*/
+}
+const font = Inter({subsets: ["latin"],})
 
-const font = Open_Sans({ subsets: ["latin"] });
 
 type Props = React.ReactNode
 
 export const metadata: Metadata = {
   title: "Buff",
   description: "Unleash your inner film buff.",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: Props;
+  children: Props
 }>) {
   return (
     <html lang="en">
