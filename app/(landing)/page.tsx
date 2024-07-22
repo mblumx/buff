7fbba4a4b2257be2265b796a82cc8c2a8ad0f90e
matthewblu421 <44147556+matthewblu421@users.noreply.github.com>
2024-07-22 14:55:48 -0400

    Finally added a hero image I actually like

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,12 @@
-const LandingPage = () => {
-  return ( <div>This is the main (landing) page</div> );
+import { Loader } from "lucide-react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] mb-8 lg:mb-0 lg:w-[422px] lg:h-[422px] ">
+        <Image className="rounded-xl" src="/hero.png" fill alt="Hero" />
+      </div>
+    </div>
+  )
 }
- 
-export default LandingPage;
