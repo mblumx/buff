18a838f35268ec61bc3567a6ccdf32816ba292f1
matthewblu421 <44147556+matthewblu421@users.noreply.github.com>
2024-07-22 12:03:05 -0400

    Header progress ie resizing, moving some css around

--- app/(landing)/header.tsx
+++ app/(landing)/header.tsx
@@ -3,22 +3,20 @@ import Image from "next/image"
 //using named export instead of default export because it is a component, not a page
 export const Header = () => {
     return (
-        <header className="h-20 w-full border-b-2 border-slate-200 px-4">
-            <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full bg-red-500">
-                <div className="flex items-center justify-between pl-2 font-bold text-white uppercase">
-                    <Image
-                    src={"/popcorn.svg"}
-                    alt="Popcorn Logo"
-                    height={40}
-                    width={40}
-                    />
-                    <div>
-                        Buff
-                    </div>
-
-                </div>
-            </div>
-        </header>
+      <header className="h-20 w-full border-b-2 border-slate-200 px-4">
+        <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full bg-red-500">
+          {/* <div className="flex items-center justify-between pl-2 font-bold text-white uppercase"> */}
+          <div className="pt-8 pl-4 pb-7 flex items-center gap-x-2 ">
+            <Image
+              src={"/popcorn.svg"}
+              alt="Popcorn Logo"
+              height={35}
+              width={35}
+            />
+            <h1 className="text-2xl font-extrabold text-white tracking-wide">Buff</h1>
+          </div>
+        </div>
+      </header>
     )
 
 }
